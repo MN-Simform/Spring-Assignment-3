1524b01f5bd1bbd6b5e42032f3b2c45863447908
MN-Simform <nimit@simformsolutions.com>
2023-04-28 17:46:02 +0530

    Add Spring Assignmnet 3

---
+++ src/main/java/com/spring/assignment/SpringAssignmentThree.java
@@ -0,0 +1,13 @@
+package com.spring.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssignmentThree {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssignmentThree.class, args);
+	}
+
+}
